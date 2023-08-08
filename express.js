618a26f9c6416e9de8a47d0e69293efbcc6dae93
pradeep <pradeep.kumar@zysk.tech>
2023-08-08 15:27:57 +0530

    first test commit

---
+++ express.js
@@ -0,0 +1,103 @@
+const express = require("express");
+
+let app = express();
+
+app.use(express.json());
+
+const port = 4000;
+
+//get all
+app.get("/users", async (req, res) => {
+  let response = await fetch("http://localhost:5000/content");
+  let data = await response.json();
+  if (!response.ok) {
+    return res.json({ error: "Error while fetching data" });
+  }
+  res.json(data);
+});
+
+//get one
+app.get("/users/:id", async (req, res) => {
+  let { id } = req.params;
+  if (id <= 0) {
+    return res.json({ error: "Not a valid ID value" });
+  }
+  try {
+    let response = await fetch(`http://localhost:5000/content/${id}`);
+    let data = await response.json();
+    if (!response.ok) {
+      return res.json({ error: "error fetching the user" });
+    }
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//create
+app.post("/users", async (req, res) => {
+  let payload = req.body;
+  let response = await fetch("http://localhost:5000/content/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+  let data = await response.json();
+  if (!response.ok) {
+    return res.json({ error: "Error while adding data" });
+  }
+  res.json(data);
+});
+
+//update
+app.patch("/users/:id", async (req, res) => {
+  let { id } = req.params;
+  if (id <= 0) {
+    return res.json({ error: "Not a valid ID value" });
+  }
+  let payload = req.body;
+  try {
+    let response = await fetch(`http://localhost:5000/content/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+    let data = await response.json();
+    if (!response.ok) {
+      return res.json({ error: "Error while adding data" });
+    }
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//delete
+app.delete("/users/:id", async (req, res) => {
+  let { id } = req.params;
+  if (id <= 0) {
+    return res.status(404).json({ error: "Not a valid ID value" });
+  }
+
+  
+  try {
+    let response = await fetch(`http://localhost:5000/content/${id}`, {
+      method: "DELETE",
+    });
+    let data = await response.json();
+    if (!response.ok) {
+      return res.json({ error: "Error while deleting data" });
+    }
+    res.json({mssg:"Deleted successfully"});
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running in http://localhost:${port}`);
+});
---
+++ index.js
@@ -0,0 +1,53 @@
+let data=require("../db.json")
+
+
+// obj1={
+//     first:"kk",
+//     fullName:function(){
+//         return this.first+" "+ this.last
+//     }
+    
+// }
+// obj2={
+//     first:"Pradeep",
+//     last:"Kumar",
+// }
+
+// console.log(obj1.fullName())
+
+// let add=function(){
+//     let counter=0;
+//     function plus(){
+//         counter+=1
+//     }
+//     plus()
+//     plus()
+//     return counter
+// }
+
+// console.log(add());
+
+
+
+// let promise = new Promise(function (resolve, reject) {
+// 	const x = "geeksforgeeks";
+// 	const y = "geeksforgeek"
+// 	if (x === y) {
+// 		resolve();
+// 	} else {
+// 		reject();
+// 	}
+// });
+
+// promise.
+// 	then(function () {
+// 		console.log('Success, You are a GEEK');
+// 	}).
+// 	catch(function () {
+// 		console.log('Some error has occurred');
+// 	});
+
+console.log(data);
+
+
+
